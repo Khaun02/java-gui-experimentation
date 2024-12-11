1a7064cfd4cc6b7777295346155effc5070d6224
Khaun02 <khaunx02@gmail.com>
2024-12-10 21:23:39 -0600

    1.0.1
    Renamed files and added in JLabel
    
    - Renamed Frame.java to GUI.java
    - Added in Jlabels into the main method
    - Tried doing it all in the GUI class but it got messy

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import javax.swing.JLabel;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // if we dont need the variable, or we dont need to make changes here, we can just make the frame
+        GUI frame = new GUI();
+
+        // constructor for label, shortcut to add text is just adding inside the Jlabel()
+        JLabel label = new JLabel("Hello World");
+        //now we have to add this label to the GUI
+        frame.add(label);
+
     }
 }
